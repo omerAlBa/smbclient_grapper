dd68ecfa34d5e88fc8cbca91f5167b3607bf8cb0
omerAlBa <45664296+omerAlBa@users.noreply.github.com>
2025-03-04 21:04:30 +0100

    Update smbclient_grapper.sh

--- smbclient_grapper.sh
+++ smbclient_grapper.sh
@@ -1,33 +1,56 @@
-#!/bin/basj
+#!/bin/bash
 
+# help function
+
+if [ $1 == '--help' ];
+then
+	echo '############ Help Secction ############ '
+	echo " first argument is for targegt "
+	echo " second argument is for ad_name "
+	echo " third argument is for ad_user "
+	echo " four argument is for ad_pass "
+	exit
+fi
 
 # INIT Function
 function download_share_content {
-  #smbclient \\\\$target\\$share -N -c "ls" | while read line;
-  smbclient \\\\$target\\$share --user="active/SVC_TGS%GPPstillStandingStrong2k18" -c "ls" | while read line;
+  smbclient \\\\$target\\$share $([ -n "$user" ] && echo "--user=${user}" || echo "-N") -c "ls" | while read line;
+  #smbclient \\\\$target\\$share --user="${user}" -c "ls" | while read line;
   do
 	echo $line | grep -q -E 'anony.*|\. |\.\.'
 	command_result=$?
 	if [[ $command_result == 0 ]];
 	then
 		continue
-	fi	
+	fi
 
   	# Extract the name and type (file or folder)
 	name=$(echo "$line" | awk '{print $1}')
 	type=$(echo "$line" | awk '{print $2}')
-	
+
 	if [[ "$type" == "D" ]]; then
 		#smbclient \\\\$target\\$share -N -c "prompt OFF;recurse ON;mget *"
-		smbclient \\\\$target\\$share --user=SVC_TGS%GPPstillStandingStrong2k18 -c "prompt OFF;recurse ON;mget *"
+		smbclient \\\\$target\\$share $([ -n "$user" ] && echo "--user=${user}" || echo "-N") -c "prompt OFF;recurse ON;mget *"
+		#smbclient \\\\$target\\$share --user="${user}" -c "prompt OFF;recurse ON;mget *"
 	fi
   done
 }
 
 # INIT Variable
 # was ist das target?
-target=10.10.10.100
+target=$1
+ad_name=$2
+ad_user=$3
+ad_password=$4
 
+
+if [ -n "$ad_user" ] && [ -n "$ad_user" ] && [ -n $ad_name ];
+then
+	#create user credential
+	user="${ad_name}/${ad_user}%${ad_password}"
+fi
+
+echo $user
 # wo willst du die sachen hin haben?
 desc_path='/tmp'
 
@@ -35,8 +58,8 @@ desc_path='/tmp'
 # list all shares
 echo "[Config] traget is ${target}"
 
-#shares=$(smbclient -L \\\\$target\\ -N | grep -E '^[[:space:]]+[A-Za-z0-9_]+' | grep -v 'Sharename' | awk '{print $1}')
-shares=$(smbclient -L \\\\$target\\ --user="active/SVC_TGS%GPPstillStandingStrong2k18" | grep -E '^[[:space:]]+[A-Za-z0-9_]+' | grep -v 'Sharename' | awk '{print $1}')
+shares=$(smbclient -L \\\\$target\\ $([ -n "$user" ] && echo "--user=${user}" || echo "-N") | grep -E '^[[:space:]]+[A-Za-z0-9_]+' | grep -v 'Sharename' | awk '{print $1}')
+#shares=$(smbclient -L \\\\$target\\ --user="${user}" | grep -E '^[[:space:]]+[A-Za-z0-9_]+' | grep -v 'Sharename' | awk '{print $1}')
 command_result=$?
 
 # ist die list leer?
@@ -48,23 +71,24 @@ if [ -z "$shares" ]; then
 fi
 
 echo "[result] list of the shares:"
-for share in $share;
-do 
+for share in $shares;
+do
 	echo "  $share"
 done
 
 # get all content of the shares
 ## what to do with not allowed?
 echo '[share query]:'
-for share in $shares; 
+for share in $shares;
 do
 	#query=$(smbclient \\\\$target\\$share -N -c "ls")
-	query=$(smbclient \\\\$target\\$share --user="active/SVC_TGS%GPPstillStandingStrong2k18" -c "ls")
+	query=$(smbclient -L \\\\$target\\$share $([ -n "$user" ] && echo "--user=${user}" || echo "-N") -c "ls")
+	#query=$(smbclient \\\\$target\\$share --user="${user}" -c "ls")
 	command_result=$?
 
 	if [ $command_result -eq 0 ];
 	then
 		echo "  $share is accessible"
 		download_share_content "${query}"
-	fi	
+	fi
 done
