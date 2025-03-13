73c1297bf153b29e1e3a0613cd8ab2e7957c0ec3
k-m <k-m@no-intressing@cui-it.de>
2025-03-13 14:40:03 +0100

    [non-functional] remove comments and old code

--- smbclient_grapper.sh
+++ smbclient_grapper.sh
@@ -4,12 +4,12 @@
 
 if [ $1 == '--help' ];
 then
-	echo '############ Help Section ############ '
+	echo '############ Help Secction ############ '
 	echo " first argument is for targegt "
 	echo " second argument is for ad_name "
 	echo " third argument is for ad_user "
 	echo " four argument is for ad_pass "
-	exit
+	exit 
 fi
 
 # INIT Function
@@ -21,20 +21,13 @@ function download_share_content {
 	if [[ $command_result == 0 ]];
 	then
 		continue
-	fi
+	fi	
 
-  	# Extract the name and type (file or folder)
-	name=$(echo "$line" | awk '{print $1}')
-	type=$(echo "$line" | awk '{print $2}')
-
-	if [[ "$type" == "D" ]]; then
-		smbclient \\\\$target\\$share $([ -n "$user" ] && echo "--user=${user}" || echo "-N") -c "prompt OFF;recurse ON;mget *"
-	fi
+	smbclient \\\\$target\\$share $([ -n "$user" ] && echo "--user=${user}" || echo "-N") -c "prompt OFF;recurse ON;mget *"
   done
 }
 
 # INIT Variable
-# was ist das target?
 target=$1
 ad_name=$2
 ad_user=$3
@@ -47,18 +40,13 @@ then
 	user="${ad_name}/${ad_user}%${ad_password}"
 fi
 
-echo $user
-# wo willst du die sachen hin haben?
-desc_path='/tmp'
-
-
 # list all shares
 echo "[Config] traget is ${target}"
 
 shares=$(smbclient -L \\\\$target\\ $([ -n "$user" ] && echo "--user=${user}" || echo "-N") | grep -E '^[[:space:]]+[A-Za-z0-9_]+' | grep -v 'Sharename' | awk '{print $1}')
 command_result=$?
 
-# ist die list leer?
+# is the list empty?
 if [ -z "$shares" ]; then
 	echo 'not able to fetch or list is empty!'
 	echo " command result is: $command_result"
@@ -68,14 +56,14 @@ fi
 
 echo "[result] list of the shares:"
 for share in $shares;
-do
+do 
 	echo "  $share"
 done
 
 # get all content of the shares
 ## what to do with not allowed?
 echo '[share query]:'
-for share in $shares;
+for share in $shares; 
 do
 	query=$(smbclient -L \\\\$target\\$share $([ -n "$user" ] && echo "--user=${user}" || echo "-N") -c "ls")
 	command_result=$?
@@ -84,5 +72,5 @@ do
 	then
 		echo "  $share is accessible"
 		download_share_content "${query}"
-	fi
+	fi	
 done
